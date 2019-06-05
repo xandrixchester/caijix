fd8069193318225a8c0b02f40fed3ff8ccf2e53c
xandrix <xandrixecklor@hotmaill.com>
2019-06-05 02:58:57 +0000

    Interest Submission

--- Gemfile
+++ Gemfile
@@ -63,7 +63,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'jquery-rails'
-
+gem 'simple_form'
 
 
 
--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -4,4 +4,21 @@ class InterestsController < ApplicationController
 		@interests = Interest.all
 	end
 
+
+	def new
+		@interest = Interest.new
+	end
+
+	def create
+		Interest.create(interest_params)
+		redirect_to root_path
+	end
+
+
+	private
+
+	def interest_params
+		params.require(:interest).permit(:title, :description)
+	end
+
 end
