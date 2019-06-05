645114ad8c1f74c5aeaa668fd14df24874a5542b
xandrix <xandrixecklor@hotmaill.com>
2019-06-05 02:23:36 +0000

    Interest on main page

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,6 +1,7 @@
 class InterestsController < ApplicationController
 
 	def index
+		@interests = Interest.all
 	end
-	
+
 end
---
+++ db/migrate/20190605021301_create_interests.rb
@@ -0,0 +1,9 @@
+class CreateInterests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :interests do |t|
+    	t.string :title
+    	t.text :description
+      t.timestamps
+    end
+  end
+end
